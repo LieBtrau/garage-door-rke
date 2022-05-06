fa6f6cffb3c779c19d3956c087dd52738c9d6a43
Christoph Tack <prog.send@gmail.com>
2022-05-06 21:54:00 +0200

    RadioHead works with Lolin-C3 MINI

--- firmware/tests/lolin-c3-mini_Radiohead/src/main.cpp
+++ firmware/tests/lolin-c3-mini_Radiohead/src/main.cpp
@@ -1,8 +1,8 @@
-// rf69_reliable_datagram_client.pde
+// rf69_reliable_datagram_server.pde
 // -*- mode: C++ -*-
-// Example sketch showing how to create a simple addressed, reliable messaging client
+// Example sketch showing how to create a simple addressed, reliable messaging server
 // with the RHReliableDatagram class, using the RH_RF69 driver to control a RF69 radio.
-// It is designed to work with the other example rf69_reliable_datagram_server
+// It is designed to work with the other example rf69_reliable_datagram_client
 // Tested on Moteino with RFM69 http://lowpowerlab.com/moteino/
 // Tested on miniWireless with RFM69 www.anarduino.com/miniwireless
 // Tested on Teensy 3.1 with RF69 on PJRC breakout board
@@ -17,30 +17,30 @@
 /**
  * @brief 
  * 
- * Signal   NODEMCU32-S-pin
- * NSS      GPIO5
- * DIO0     GPIO4
- * MISO     GPIO19
- * MOSI     GPIO23
- * SCK      GPIO18
+ * Signal   LOLIN C3-MINI
+ * SCK      10
+ * MISO     4
+ * MOSI     7
+ * NSS      6
+ * DIO0     2 
  * 3V3		3V3
  * GND		GND
  */
 
 // Singleton instance of the radio driver
-RH_RF69 driver(5, 4);
-// RH_RF69 driver(15, 16); // For RF69 on PJRC breakout board with Teensy 3.1
-// RH_RF69 driver(4, 2); // For MoteinoMEGA https://lowpowerlab.com/shop/moteinomega
-// RH_RF69 driver(8, 7); // Adafruit Feather 32u4
+RH_RF69 driver(6);
 
 // Class to manage message delivery and receipt, using the driver declared above
-RHReliableDatagram manager(driver, CLIENT_ADDRESS);
-uint8_t data[] = "Hello World!";
+RHReliableDatagram manager(driver, SERVER_ADDRESS);
+
+uint8_t data[] = "And hello back to you";
 // Dont put this on the stack:
 uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
+const int LED_BUILTIN = 7;
 
 void setup()
 {
+	pinMode(LED_BUILTIN, OUTPUT);
 	Serial.begin(115200);
 	while (!Serial)
 		;
@@ -56,32 +56,29 @@ void setup()
 	// ishighpowermodule flag set like this:
 	driver.setFrequency(868.0);
 	driver.setTxPower(0, true);
+	Serial.println("server ready");
 }
-
 
 void loop()
 {
-	Serial.println("Sending to rf69_reliable_datagram_server");
-
-	// Send a message to manager_server
-	if (manager.sendtoWait(data, sizeof(data), SERVER_ADDRESS))
+	if (manager.available())
 	{
-		// Now wait for a reply from the server
+		// Wait for a message addressed to us from the client
 		uint8_t len = sizeof(buf);
 		uint8_t from;
-		if (manager.recvfromAckTimeout(buf, &len, 2000, &from))
+		if (manager.recvfromAck(buf, &len, &from))
 		{
-			Serial.print("got reply from : 0x");
+			Serial.print("got request from : 0x");
 			Serial.print(from, HEX);
 			Serial.print(": ");
 			Serial.println((char *)buf);
-		}
-		else
-		{
-			Serial.println("No reply, is rf69_reliable_datagram_server running?");
+
+			// Send a reply back to the originator client
+			if (!manager.sendtoWait(data, sizeof(data), from))
+			{
+				Serial.println("sendtoWait failed");
+			}
+			digitalWrite(LED_BUILTIN, digitalRead(LED_BUILTIN) == HIGH ? LOW : HIGH);
 		}
 	}
-	else
-		Serial.println("sendtoWait failed");
-	delay(500);
 }
