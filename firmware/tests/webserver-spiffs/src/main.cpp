337722253c4e2c850239c5933622a770d2c93c5c
Christoph Tack <prog.send@gmail.com>
2022-06-05 21:49:39 +0200

    Setting static IP

--- firmware/tests/webserver-spiffs/src/main.cpp
+++ firmware/tests/webserver-spiffs/src/main.cpp
@@ -5,9 +5,6 @@
   https://randomnerdtutorials.com/esp32-vs-code-platformio-spiffs/
 
   Lolin-C3 doesn't connect to WiFi.  Possible causes : LDO too weak, not enough bulk cap.
-	Patches needed:
-		WebAuthentication.cpp:74			mbedtls_md5_starts_ret(&_ctx);
-		AsyncWebSocket.cpp:840		        return IPAddress((uint32_t)0U);
 
 *********/
 
@@ -28,68 +25,81 @@ String ledState;
 AsyncWebServer server(80);
 
 // Replaces placeholder with LED state value
-String processor(const String& var){
-  Serial.println(var);
-  if(var == "STATE"){
-    if(digitalRead(ledPin)){
-      ledState = "ON";
-    }
-    else{
-      ledState = "OFF";
-    }
-    Serial.print(ledState);
-    return ledState;
-  }
-  return String();
+String processor(const String &var)
+{
+	Serial.println(var);
+	if (var == "STATE")
+	{
+		if (digitalRead(ledPin))
+		{
+			ledState = "ON";
+		}
+		else
+		{
+			ledState = "OFF";
+		}
+		Serial.print(ledState);
+		return ledState;
+	}
+	return String();
 }
- 
-void setup(){
-  // Serial port for debugging purposes
-  Serial.begin(115200);
-  pinMode(ledPin, OUTPUT);
 
-  // Initialize SPIFFS
-  if(!SPIFFS.begin(true)){
-    Serial.println("An Error has occurred while mounting SPIFFS");
-    return;
-  }
+void setup()
+{
+	// Serial port for debugging purposes
+	Serial.begin(115200);
+	pinMode(ledPin, OUTPUT);
 
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
-  }
+	// Initialize SPIFFS
+	if (!SPIFFS.begin(true))
+	{
+		Serial.println("An Error has occurred while mounting SPIFFS");
+		return;
+	}
 
-  // Print ESP32 Local IP Address
-  Serial.println(WiFi.localIP());
+	IPAddress local_IP(192, 168, 1, 253);
+	IPAddress gateway(192, 168, 1, 1);
+	IPAddress subnet(255, 255, 255, 0);
+	if (!WiFi.config(local_IP, gateway, subnet))
+	{
+		Serial.println("STA Failed to configure");
+	}
 
-  // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/index.html", String(), false, processor);
-  });
-  
-  // Route to load style.css file
-  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/style.css", "text/css");
-  });
+	// Connect to Wi-Fi
+	WiFi.begin(ssid, password);
+	while (WiFi.status() != WL_CONNECTED)
+	{
+		delay(1000);
+		Serial.println("Connecting to WiFi..");
+	}
 
-  // Route to set GPIO to HIGH
-  server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request){
+	// Print ESP32 Local IP Address
+	Serial.println(WiFi.localIP());
+
+	// Route for root / web page
+	server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+			  { request->send(SPIFFS, "/index.html", String(), false, processor); });
+
+	// Route to load style.css file
+	server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request)
+			  { request->send(SPIFFS, "/style.css", "text/css"); });
+
+	// Route to set GPIO to HIGH
+	server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request)
+			  {
     digitalWrite(ledPin, HIGH);    
-    request->send(SPIFFS, "/index.html", String(), false, processor);
-  });
-  
-  // Route to set GPIO to LOW
-  server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request){
-    digitalWrite(ledPin, LOW);    
-    request->send(SPIFFS, "/index.html", String(), false, processor);
-  });
+    request->send(SPIFFS, "/index.html", String(), false, processor); });
 
-  // Start server
-  server.begin();
+	// Route to set GPIO to LOW
+	server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request)
+			  {
+    digitalWrite(ledPin, LOW);    
+    request->send(SPIFFS, "/index.html", String(), false, processor); });
+
+	// Start server
+	server.begin();
 }
- 
-void loop(){
-  
+
+void loop()
+{
 }
