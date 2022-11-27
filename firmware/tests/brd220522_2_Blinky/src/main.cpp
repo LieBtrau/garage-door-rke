5b7162f5ecf984651cae73cf50f47074c1030e13
Christoph Tack <prog.send@gmail.com>
2022-11-27 19:31:15 +0100

    Adding LED example for R2 (with patched LEDs)

---
+++ firmware/tests/brd220522_2_Blinky/src/main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+
+const int led_green = 8;
+const int led_red = 4;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led_green, OUTPUT);
+  pinMode(led_red, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led_green, HIGH);
+  digitalWrite(led_red, LOW);
+  delay(500);
+  digitalWrite(led_green, LOW);
+  digitalWrite(led_red, HIGH);
+  delay(500);
+}
