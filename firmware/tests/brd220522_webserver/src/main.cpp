eb577b4e6320b5e24882725650e1712d8ea7c08e
Christoph Tack <prog.send@gmail.com>
2022-12-24 16:42:25 +0100

    webserver for pin form

--- firmware/tests/brd220522_webserver/src/main.cpp
+++ firmware/tests/brd220522_webserver/src/main.cpp
@@ -1,7 +1,11 @@
-/*********
-  Rui Santos
-  Complete project details at https://randomnerdtutorials.com/esp32-web-server-spiffs-spi-flash-file-system/
-*********/
+/** \file brd220522_webserver
+ * 
+ * \author Christoph Tack
+ * \date 2022-12-24
+ * \brief Webserver for access control with pin code
+ * \details When browsing to the webserver, pin_form.html is served to the client.  The client fills in the pin code and generates
+ * a HTTP-POST message.  The server checks the pin code and send a dynamically generated webpage (door.html) back to the client.
+ */
 
 // Import required libraries
 #include "WiFi.h"
@@ -13,28 +17,33 @@
 const int ledPin = 2;
 // Stores LED state
 String ledState;
+bool isAccessGranted=false;
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 
-// Replaces placeholder with LED state value
 String processor(const String &var)
 {
   Serial.println(var);
-  if (var == "STATE")
+  if (var == "DOOR_STATE")
   {
-    if (digitalRead(ledPin))
-    {
-      ledState = "ON";
-    }
-    else
-    {
-      ledState = "OFF";
-    }
-    Serial.print(ledState);
-    return ledState;
+    return isAccessGranted ? "open" : "blijft gesloten";
   }
   return String();
+}
+
+void action(AsyncWebServerRequest *request)
+{
+  int params = request->params();
+  for (int i = 0; i < params; i++)
+  {
+    AsyncWebParameter *p = request->getParam(i);
+    if (p->name() == "pincode" && p->value() == PIN_CODE)
+    {
+      isAccessGranted = true;
+    }
+  }
+  request->send(SPIFFS, "/door.html", String(), false, processor);
 }
 
 void setup()
@@ -50,16 +59,16 @@ void setup()
     return;
   }
 
+  // Configures static IP address
   IPAddress local_IP(192, 168, 1, 253); // Set your Static IP address
   IPAddress gateway(192, 168, 1, 1);    // Set your Gateway IP address
   IPAddress subnet(255, 255, 255, 0);
-  // Configures static IP address
   if (!WiFi.config(local_IP, gateway, subnet))
   {
     Serial.println("STA Failed to configure");
   }
   // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
+  WiFi.begin(SSID, PASSWORD);
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(1000);
@@ -71,26 +80,10 @@ void setup()
 
   // Route for root / web page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send(SPIFFS, "/index.html", String(), false, processor); });
+            { request->send(SPIFFS, "/pin_form.html", String(), false); });
 
-  // Route to load style.css file
-  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send(SPIFFS, "/style.css", "text/css"); });
-
-  // Route to set GPIO to HIGH
-  server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request)
-            {
-    digitalWrite(ledPin, HIGH);    
-    request->send(SPIFFS, "/index.html", String(), false, processor); });
-
-  // Route to set GPIO to LOW
-  server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request)
-            {
-    digitalWrite(ledPin, LOW);    
-    request->send(SPIFFS, "/index.html", String(), false, processor); });
-
-  // Start server
-  server.begin();
+  server.on("/login", HTTP_POST, action);
+  server.begin(); // Start server
 }
 
 void loop()
